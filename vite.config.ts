33f7a274176b1e4dd93c73eeb8515a5cab2f24ae
Hubujiu <3106282063@qq.com>
2026-07-11 09:36:17 +0800

    chore: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "pages" ? "/cover-atelier/" : "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@jsquash/avif"],
@@ -9,4 +10,4 @@ export default defineConfig({
   worker: {
     format: "es",
   },
-});
+}));
